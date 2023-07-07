20b5074ad6baa57aa7bd8b098f57068c9c3f6342
Tony Kamillo <tonykamillo@gmail.com>
2023-07-07 18:55:42 -0300

    Making sure the dataset are being loaded

--- challenge.py
+++ challenge.py
@@ -15,3 +15,32 @@ def load_csv(filename):
         reader = csv.DictReader(csvfile)
         items = [row for row in reader]
     return items
+
+
+def dump_csv(filename, data):
+    """Creates csv file into data/output/ folder"""
+    with open(os.path.join(DS_OUTPUT_DIR, filename), "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
+        writer.writeheader()
+        for item in data:
+            writer.writerow(item)
+
+
+def main():
+    # Loading csv datasets
+    bills = load_csv("bills.csv")
+    legislators = load_csv("legislators.csv")
+    vote_results = load_csv("vote_results.csv")
+    votes = load_csv("votes.csv")
+
+    vote2bill = {bill_vote["id"]: bill_vote["bill_id"] for bill_vote in votes}
+
+    print(bills)
+    print(legislators)
+    print(vote_results)
+    print(votes)
+    print(vote2bill)
+
+
+if __name__ == "__main__":
+    main()
