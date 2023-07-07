27aeaaca77f62bf0201a02cffb7f05a95489c5c6
Tony Kamillo <tonykamillo@gmail.com>
2023-07-07 19:48:34 -0300

    Finishing the challenge.

--- challenge.py
+++ challenge.py
@@ -26,6 +26,38 @@ def dump_csv(filename, data):
             writer.writerow(item)
 
 
+def count_votes(legislators, bills, vote_results, vote2bill):
+    """Process vote results"""
+    for vote in vote_results:
+        _, legislator_id, vote_id, vote_type = vote.values()
+        legislator = legislators[legislator_id]
+        bill = bills[vote2bill[vote_id]]
+
+        if "primary_sponsor" not in bill:
+            bill["primary_sponsor"] = legislators.get(bill["sponsor_id"], {}).get(
+                "name", "Unknown"
+            )
+            bill.pop("sponsor_id")
+
+        if int(vote_type) == 1:
+            legislator["num_supported_bills"] += 1
+            bill["supporter_count"] = bill.get("supporter_count", 0) + 1
+
+        elif int(vote_type) == 2:
+            legislator["num_opposed_bills"] += 1
+            bill["opposer_count"] = bill.get("opposer_count", 0) + 1
+
+    return legislators.values(), bills.values()
+
+
+def add_legislator_fields(leg):
+    """Adds accounting fields for legislator in order to avoid empty values for both num_supported_bills and num_opposed_bills fields"""
+    leg["num_supported_bills"] = 0
+    leg["num_opposed_bills"] = 0
+
+    return leg
+
+
 def main():
     # Loading csv datasets
     bills = load_csv("bills.csv")
@@ -33,13 +65,20 @@ def main():
     vote_results = load_csv("vote_results.csv")
     votes = load_csv("votes.csv")
 
+    # Transforming some datasets into dictionary to constant time O(1) access.
+    # It gonna allows to make only one iteration over vote_results making the account in linear time O(n)
     vote2bill = {bill_vote["id"]: bill_vote["bill_id"] for bill_vote in votes}
+    legislators_dict = {leg["id"]: add_legislator_fields(leg) for leg in legislators}
+    bills_dict = {bill["id"]: bill for bill in bills}
 
-    print(bills)
-    print(legislators)
-    print(vote_results)
-    print(votes)
-    print(vote2bill)
+    # Accounting votes for both legislators and bills
+    legislator_results, bill_results = count_votes(
+        legislators_dict, bills_dict, vote_results, vote2bill
+    )
+
+    # Saving accounts files
+    dump_csv("legislators-support-oppose-count.csv", list(legislator_results))
+    dump_csv("bills.csv", list(bill_results))
 
 
 if __name__ == "__main__":
